14a81f9f27683639874a71546cce10af38de4bdc
Abdalrhman-Almarakeby <aalmarakeby@gmail.com>
2023-09-06 00:07:44 +0300

    Add new break points and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        BarlowCondensed: "'Barlow Condensed', sans-serif",
+      },
+      backgroundImage: {
+        "background-home-desktop":
+          "url('./src/assets/home/background-home-desktop.jpg')",
+        "background-home-tablet":
+          "url('./src/assets/home/background-home-tablet.jpg')",
+        "background-home-mobile":
+          "url('./src/assets/home/background-home-mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
