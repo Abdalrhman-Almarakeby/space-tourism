c4bb931cd38c2cf1406c32e0f9c0ccc587f355c2
Abdalrhman <aalmarakeby@gmail.com>
2023-09-13 04:39:46 +0300

    Move the routing to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Destination from "./pages/Destination";
-import Crew from "./pages/Crew";
-import Technology from "./pages/Technology";
+import Page from "./router";
 import "./CSS/main.css";
 
 export default function App() {
-  let page;
-  switch (window.location.pathname) {
-    case "/":
-      page = <Home />;
-      break;
-    case "/destination":
-      page = <Destination />;
-      break;
-    case "/crew":
-      page = <Crew />;
-      break;
-    case "/technology":
-      page = <Technology />;
-      break;
-  }
   return (
     <div
-      className={`min-h-screen flex flex-col relative bg-cover bg-center bg-background-home-mobile md:bg-background-home-tablet xl:bg-background-home-desktop overflow-hidden`}
+      className={`min-h-screen flex flex-col relative bg-slate-800 bg-cover bg-center bg-background-home-mobile md:bg-background-home-tablet xl:bg-background-home-desktop overflow-hidden`}
     >
       <Navbar />
-      {page}
+      <Page />
     </div>
   );
 }
