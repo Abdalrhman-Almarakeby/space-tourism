6b06b8c02c2e9ee655af379c8229456ec53620dc
Abdalrhman <aalmarakeby@gmail.com>
2023-09-22 02:50:12 +0300

    Rename the JS folder to utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
-import Page from "./JS/router";
+import Page from "./utils/router";
 import "./CSS/main.css";
-import { styleObject, backgroundFunc } from "./JS/backgroundImg";
+import { styleObject, backgroundFunc } from "./utils/backgroundImg";
 
 export default function App() {
   return (
